07367ec57502d12c6417146ec4c1653483116d2c
Horacio Belardita <horaciobelardita@gmail.com>
2026-05-06 18:05:57 -0300

    fix(db): update permissions for clinica_user and enhance test database initialization script

--- tp-final-integrador/init/00-create-test-db.sql
+++ tp-final-integrador/init/00-create-test-db.sql
@@ -1,5 +1,8 @@
 CREATE DATABASE IF NOT EXISTS prog3_turnos_test;
--- Nos aseguramos de que los posibles usuarios tengan permisos
-GRANT ALL PRIVILEGES ON prog3_turnos_test.* TO 'root'@'%';
+
+-- Otorgar permisos al usuario clinica_user sobre ambas bases de datos
+-- (Por si el entrypoint de Docker solo le dio a la principal)
+GRANT ALL PRIVILEGES ON prog3_turnos.* TO 'clinica_user'@'%';
 GRANT ALL PRIVILEGES ON prog3_turnos_test.* TO 'clinica_user'@'%';
+
 FLUSH PRIVILEGES;
