32b2d65ee48596f273c7e144775d20468fd89a48
tberon <tomiberon03@gmail.com>
2026-06-12 18:57:38 -0300

    formateo-archivoMulter

--- tp-final-integrador/src/middlewares/multer.middleware.js
+++ tp-final-integrador/src/middlewares/multer.middleware.js
@@ -11,11 +11,11 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     if (!fs.existsSync(UPLOADS_DIR)) fs.mkdirSync(UPLOADS_DIR, { recursive: true });
     cb(null, UPLOADS_DIR);
-    },
-    filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + '-' + file.originalname);
-    }
+  },
 });
 
 export const upload = multer({ storage });
@@ -25,19 +25,20 @@ const fileFilter = (req, file, cb) => {
   const mimetype = fileTypes.test(file.mimetype); //
   const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
 
-  if (mimetype && extname) { 
+  if (mimetype && extname) {
     return cb(null, true);
   }
-  cb(new AppError(
-          ERROR_CODES.UNSUPPORTED_MEDIA_TYPE,
-          `Tipo de archivo no permitido. Solo se permiten: ${fileTypes}`,
-        ),
-        false,
-      );
+  cb(
+    new AppError(
+      ERROR_CODES.UNSUPPORTED_MEDIA_TYPE,
+      `Tipo de archivo no permitido. Solo se permiten: ${fileTypes}`,
+    ),
+    false,
+  );
 };
 
-export const uploadImage = multer({ 
+export const uploadImage = multer({
   storage: storage,
-  limits: { fileSize: MAX_FILE_SIZE }, 
-  fileFilter: fileFilter
+  limits: { fileSize: MAX_FILE_SIZE },
+  fileFilter: fileFilter,
 });
